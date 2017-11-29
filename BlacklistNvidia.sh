dbee47f0d51bfc0d1d6ced5c5bdfbc8d2caf4e56
Superdanby <dannymrt1@gmail.com>
2017-11-29 09:57:54 +0800

    Add nvidia_uvm to modprobe.blacklist

--- BlacklistNvidia.sh
+++ BlacklistNvidia.sh
@@ -1,3 +1,5 @@
+# https://github.com/Superdanby/Grub-Nvidia-Entry
+
 Curnel=`uname -r`
 if [[ `sudo sed -n '/^menuentry/,/}/p;' /boot/efi/EFI/fedora/grub.cfg | sed '/}/q' | grep $Curnel` == '' ]];then
     printf "You are not on the latest kernel!\n"
@@ -13,11 +15,11 @@ fi
 printf "Configuring GRUB Menu...\n"
 printf "Original boot options with Nvidia modules disabled:\n"
 OldKerPara=`sudo cat /etc/default/grub | grep GRUB_CMDLINE`
-if [[ `sudo cat /etc/default/grub | grep GRUB_CMDLINE | grep modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset` == '' ]]; then
+if [[ `sudo cat /etc/default/grub | grep GRUB_CMDLINE | grep modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm` == '' ]]; then
     Nline=`sudo grep -n GRUB_CMDLINE /etc/default/grub | cut -d : -f 1`
     # KernelPara="${OldKerPara::-1} modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset\""
     printf "GRUB_CMDLINE is at line $Nline.\n"
-    sudo sed -i "${Nline}s/\"/\ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset\"/2" /etc/default/grub
+    sudo sed -i "${Nline}s/\"/\ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\"/2" /etc/default/grub
 else
     printf "modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset is already in the boot options.:\n$OldKerPara\n"
 fi
@@ -36,7 +38,7 @@ exec tail -n +3 \$0
 # This file provides an easy way to add custom menu entries.  Simply type the
 # menu entries you want to add after this comment.  Be careful not to change
 # the 'exec tail' line above.
-`sudo sed -n '/^menuentry/,/}/p;' /boot/efi/EFI/fedora/grub.cfg | sed '/}/q' | sed 's/modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset//'`" | sudo tee /etc/grub.d/40_custom
+`sudo sed -n '/^menuentry/,/}/p;' /boot/efi/EFI/fedora/grub.cfg | sed '/}/q' | sed 's/modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm//'`" | sudo tee /etc/grub.d/40_custom
 
 if [[ `sudo cat /etc/grub.d/40_custom | grep rd.driver.blacklist=nouveau` == '' ]]; then
     sudo sed -i '/vmlinuz/s/$/ rd.driver.blacklist=nouveau/' /etc/grub.d/40_custom
